92c7798666e8cdffd92a0a55f9bbcf41cc48b6d2
tarushm <tarush@gmail.com>
2016-07-20 16:31:20 -0700

    call by name

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.post('/webhook/', function (req, res) {
 				isHungry = isHungry || (text.indexOf(key[j]) > -1);
 			}
 			if (isHungry) {
-				sendTextMessage(sender, 'Here\'s some food for you!')
+				sendTextMessage(sender, 'Here\'s some food for you,'+sender)
 				sendFoodCards(sender,randFood(),randFood(),randFood())
 			}
 		}
