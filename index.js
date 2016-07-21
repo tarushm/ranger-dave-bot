9640582e5d17d814118837d7511cdf3eef0fea46
tarushm <tarush@gmail.com>
2016-07-21 02:33:04 -0700

    new thing

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ app.post('/webhook/', function (req, res) {
     var sender = event.sender.id;
     if (event.message && event.message.text) {
       var location = event.message.text;
-      var witAIEndpoint = 'https://api.wit.ai/message?v=20141022&q=' + location;
+      var witAIEndpoint = 'https://api.wit.ai/message?v=20160721&q=' + location;
       var options = {
         url: witAIEndpoint,
         headers: {
@@ -90,8 +90,7 @@ app.post('/webhook/', function (req, res) {
       };
       request(options, function(error, response, body) {
         try {
-          var firstOutcome = body.outcomes.pop();
-          var entities = firstOutcome.entities;
+          var entities = body.entities;
           var location = entities.location.pop().value;
           var weatherEndpoint = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22' + location + '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
           request({
