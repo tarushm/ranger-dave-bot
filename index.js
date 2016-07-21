aeee6276b277f6100d3c213febb17dc3a0f04ccf
tarushm <tarush@gmail.com>
2016-07-21 02:00:26 -0700

    add ai

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'))
 })
-
+/*
 app.post('/webhook/', function (req, res) {
 	let messaging_events = req.body.entry[0].messaging
 	for (let i = 0; i < messaging_events.length; i++) {
@@ -71,6 +71,50 @@ app.post('/webhook/', function (req, res) {
   }
   res.sendStatus(200)
 })
+
+*/
+
+app.post('/webhook/', function (req, res) {
+  var messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    var event = req.body.entry[0].messaging[i];
+    var sender = event.sender.id;
+    if (event.message && event.message.text) {
+      var location = event.message.text;
+      var witAIEndpoint = 'https://api.wit.ai/message?v=20141022&q=' + location;
+      request({
+        url: witAIEndpoint,
+        json: true,
+        headers: {
+          'Authorization: Bearer LUKXMZ2XPSJAW6556VIOLLEDLJR7QGBN'
+        }
+      }, function(error, response, body) {
+        try {
+          var firstOutcome = body.outcomes.pop();
+          var entities = firstOutcome.entities;
+          var location = entities.location.pop().value;
+          var weatherEndpoint = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22' + location + '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+          request({
+            url: weatherEndpoint,
+            json: true
+          }, function(error, response, body) {
+            try {
+              var condition = body.query.results.channel.item.condition;
+              sendTextMessage(sender, "Today is " + condition.temp + " and " + condition.text + " in " + location);
+            } catch(err) {
+              console.error('error caught', err);
+              sendTextMessage(sender, "There was an error.");
+            }
+          });
+        } catch(err) {
+          console.error('error caught', err);
+          sendTextMessage(sender, "There was an error.");
+        }
+      });
+    }
+  }
+  res.sendStatus(200);
+});
 
 function randFood(){
 	return Math.floor(Math.random() * (77));
