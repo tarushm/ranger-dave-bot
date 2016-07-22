2342b1b0054ab8866d7cd725c6c69eb2a1734937
tarushm <tarush@gmail.com>
2016-07-22 02:27:36 -0700

    testing

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ function sendToApiAi(sender, message,id){
   }
   else if (!error && response.statusCode == 200) {
     var info = JSON.parse(body);
-    sendTextMessage(sender,'message received');
+    sendTextMessage(sender,bands.band[id].stage);
     }
   }
 
