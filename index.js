10041d4c2ef07238374928970c0e57c1ea77447f
tarushm <tarush@gmail.com>
2016-07-22 02:33:42 -0700

    bands time

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ function sendToApiAi(sender, message,id){
   }
   else if (!error && response.statusCode == 200) {
     var info = JSON.parse(body);
-    sendTextMessage(sender,bands.band[id].time' on 'bands.band[id].day);
+    sendTextMessage(sender,bands.band[id].time+' on '+bands.band[id].day);
     }
   }
 
