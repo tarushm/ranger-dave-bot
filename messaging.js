cafb37ffe8f4a6c7af2683648f6df8b4d5a9c2e6
tarushm <tarush@gmail.com>
2016-07-23 15:51:32 -0700

    added use strict

--- messaging.js
+++ messaging.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const request = require('request');
 
 const token = "EAAO4Pbcmmj0BAF92LgbfehLojrlthke5Wv2J53g96YsFlNpf9HhlrCETKxJfCG4IHS8TxQHbFdHhF6YG9DNlYfuMFELQvUgUxle9RCSF8uvKvhwl9d6sKZBmF4PARA9j9GiHUQtwZC2zVi86fD8ZCoENAGY53ar7DcLKhHxagZDZD";
