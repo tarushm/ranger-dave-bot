d54faa4d53fd572f1f898742dc61a9b9562d20a1
tarushm <tarush@gmail.com>
2016-07-23 20:59:57 -0700

    maps.js not working

--- maps.js
+++ maps.js
@@ -7,24 +7,28 @@ var directions = require('./directions.json')
 const token = "EAAO4Pbcmmj0BAF92LgbfehLojrlthke5Wv2J53g96YsFlNpf9HhlrCETKxJfCG4IHS8TxQHbFdHhF6YG9DNlYfuMFELQvUgUxle9RCSF8uvKvhwl9d6sKZBmF4PARA9j9GiHUQtwZC2zVi86fD8ZCoENAGY53ar7DcLKhHxagZDZD";
 
 function sendDirections(sender,bandID,stageID){
-  var destination;
-  if (bandID == "") {
-    destination = directions.directions[stageID].stage;
-  }
-  else {
-    destination = bands.band[bandID].name;
-  }
   let messageData = {
     "attachment": {
       "type": "template",
       "payload": {
+        "template_type": "generic",
+        "elements":[{
+          "title": bands.band[bandID].name,
+          "subtitle": bands.band[bandID].stage + ', ' + bands.band[bandID].day + ' at ' + bands.band[bandID].start_time,
+          "image_url": bands.band[bandID].img,
           "buttons":[{
             "type": "web_url",
-            "url": directions.directions[stageID].url,
-            "title": 'Take me there!'
+            "url": bands.band[bandID].website_url,
+            "title": 'Artist Website'
+          },
+          {
+            "type": "web_url",
+            "url": bands.band[bandID].url,
+            "title": 'Add to Schedule'
           }
           ]
-        }
+        }]
+      }
     }
   }
   request({
--- messaging.js
+++ messaging.js
@@ -5,7 +5,7 @@ const request = require('request');
 const token = "EAAO4Pbcmmj0BAF92LgbfehLojrlthke5Wv2J53g96YsFlNpf9HhlrCETKxJfCG4IHS8TxQHbFdHhF6YG9DNlYfuMFELQvUgUxle9RCSF8uvKvhwl9d6sKZBmF4PARA9j9GiHUQtwZC2zVi86fD8ZCoENAGY53ar7DcLKhHxagZDZD";
 
 function sendTextMessage(sender, text) {
-        console.log(sender + ": " + text);
+    console.log(sender + ": " + text);
 	let messageData = { text:text }
 	request({
 		url: 'https://graph.facebook.com/v2.6/me/messages',
