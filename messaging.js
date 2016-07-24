46d18e38aa1be69fd651c788219c728ae8466343
Daniel Pyrathon <daniel@coffeemeetsbagel.com>
2016-07-23 19:12:20 -0700

    fix

--- messaging.js
+++ messaging.js
@@ -5,6 +5,7 @@ const request = require('request');
 const token = "EAAO4Pbcmmj0BAF92LgbfehLojrlthke5Wv2J53g96YsFlNpf9HhlrCETKxJfCG4IHS8TxQHbFdHhF6YG9DNlYfuMFELQvUgUxle9RCSF8uvKvhwl9d6sKZBmF4PARA9j9GiHUQtwZC2zVi86fD8ZCoENAGY53ar7DcLKhHxagZDZD";
 
 function sendTextMessage(sender, text) {
+        console.log(sender + ": " + text);
 	let messageData = { text:text }
 	request({
 		url: 'https://graph.facebook.com/v2.6/me/messages',
@@ -15,11 +16,11 @@ function sendTextMessage(sender, text) {
 			message: messageData,
 		}
 	}, function(error, response, body) {
-		if (error) {
-			console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error)
-		}
+		//if (error) {
+			//console.log('Error sending messages: ', error)
+		//} else if (response.body.error) {
+			//console.log('Error: ', response.body.error)
+		//}
 	})
 }
 
--- rater.js
+++ rater.js
@@ -1,8 +1,44 @@
+'use strict'
+
+var redis = require('redis');
+var sendTextMessage = require('./messaging.js').sendTextMessage;
+const redisClient = redis.createClient(process.env.REDIS_URL);
+
+function get_rating_for_artist(artist_idx) {
+    return new Promise(function(success, fail) {
+        redisClient.multi([
+            ["hget", "artist_rating_score", artist_idx],
+            ["hget", "artist_rating_count", artist_idx],
+        ]).exec(function(err, replies) {
+            let score = parseFloat(replies[0]);
+            let count = parseFloat(replies[1]);
+            if (isNaN(score) || isNaN(count)) {
+                success({success: false});
+            } else if (count == 0) {
+                success({success: false });
+            } else {
+                success({success: true, amount: score/count });
+            }
+        });
+    });
+}
 
 function process_rating(sender, parsedJson) {
-    console.log("WOO!");
+    new Promise(function(success, fail) {
+        let params = parsedJson.result.parameters;
+
+        redisClient.multi([
+            ["hincrby", "artist_rating_score", params.bands, parseInt(params.rating)],
+            ["hincrby", "artist_rating_count", params.bands, 1],
+        ]).exec(function(err, replies) {
+            success();
+        });
+    }).then(function() {
+        sendTextMessage(sender, "Thanks for letting us know!");
+    });
 }
 
 module.exports = {
+    get_rating_for_artist: get_rating_for_artist,
     process_rating: process_rating
 };
