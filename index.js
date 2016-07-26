7bc6cf1c48c9e0815a41c3431fdfbc3ef9b719b0
tarushm <tarush@gmail.com>
2016-07-26 15:14:42 -0700

    lets see where its breaking

--- index.js
+++ index.js
@@ -60,6 +60,7 @@ app.get('/webhook/', function (req, res) {
 })
 
 app.post('/webhook/', function (req, res) {
+  console.log('Posting webhook')
   let messaging_events = req.body.entry[0].messaging
   for (let i = 0; i < messaging_events.length; i++) {
     let event = req.body.entry[0].messaging[i]
