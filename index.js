39021eb743784599f423ce6f34c4a47566eae6bd
tarushm <tarush@gmail.com>
2016-07-26 15:17:14 -0700

    lets see where its breaking

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-  res.send('Hello world, I am a chat bot')
+  res.send('Hello world, I am a chat bt')
 })
 
 // for Facebook verification
